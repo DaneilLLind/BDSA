e35fe72b465105a3ba71e0b2b62e5be0bc891b19
DaneilLLind <90045006+DaneilLLind@users.noreply.github.com>
2021-09-06 10:39:34 +0200

    Tested and implemented IsLeapYear
    * Added Program and Test class
    
    * Tested and implemented IsLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -3,12 +3,48 @@ using Xunit;
 
 namespace MyApp.Tests
 {
+    
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void LeapYearEveryFourthYear()
         {
+            var expected = true;
+            var actual = Program.IsLeapYear(4);
+            Assert.Equal(expected,actual);
 
+            expected = false;
+            actual = Program.IsLeapYear(277);
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void LeapYearNotEveryHundrethYear()
+        {
+            var expected = false;
+            var actual = Program.IsLeapYear(100);
+            Assert.Equal(expected,actual);
+
+            expected = true;
+            actual = Program.IsLeapYear(160);
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void LeapYearEveryFourHundrethYearAnyway()
+        {
+            var expected = true;
+            var actual = Program.IsLeapYear(400);
+            Assert.Equal(expected,actual);
+            for (int i = 1; i < 100; i++)
+            {
+                actual = Program.IsLeapYear(i*400);
+                Assert.Equal(expected,actual);
+            }
+
+            expected = false;
+            actual = Program.IsLeapYear(500);
+            Assert.Equal(expected,actual);
         }
     }
 }
