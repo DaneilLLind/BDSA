87d9c1fad8d32fc24f443f8daad82de5b6db857b
Daniel <dlli@itu.dk>
2021-09-09 22:04:58 +0200

    Implemented Error Handling

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -11,11 +11,11 @@ namespace MyApp.Tests
         public void LeapYearEveryFourthYear()
         {
             var expected = true;
-            var actual = Program.IsLeapYear(4);
+            var actual = Program.IsLeapYear(2004);
             Assert.Equal(expected,actual);
 
             expected = false;
-            actual = Program.IsLeapYear(277);
+            actual = Program.IsLeapYear(2277);
             Assert.Equal(expected,actual);
         }
 
@@ -23,11 +23,11 @@ namespace MyApp.Tests
         public void LeapYearNotEveryHundrethYear()
         {
             var expected = false;
-            var actual = Program.IsLeapYear(100);
+            var actual = Program.IsLeapYear(1000);
             Assert.Equal(expected,actual);
 
             expected = true;
-            actual = Program.IsLeapYear(160);
+            actual = Program.IsLeapYear(1600);
             Assert.Equal(expected,actual);
         }
         
@@ -37,7 +37,7 @@ namespace MyApp.Tests
             var expected = true;
             var actual = Program.IsLeapYear(400);
             Assert.Equal(expected,actual);
-            for (int i = 1; i < 100; i++)
+            for (int i = 4; i < 100; i++)
             {
                 actual = Program.IsLeapYear(i*400);
                 Assert.Equal(expected,actual);
@@ -64,6 +64,30 @@ namespace MyApp.Tests
             var expected = "nay";
             Assert.Contains(expected,actual);
 
+        }
+
+        [Fact]
+        public void InvalidUserInput()
+        {
+            Console.SetIn(new StringReader("invalid input"));
+            var output = new StringWriter();
+            Console.SetOut(output);
+            Program.Main();
+            var actual = output.ToString();
+            var expected = "Please enter an integer";
+            Assert.Contains(expected,actual);
+        }
+
+        [Fact]
+        public void ToEarlyYear()
+        {
+            Console.SetIn(new StringReader("100"));
+            var output = new StringWriter();
+            Console.SetOut(output);
+            Program.Main();
+            var actual = output.ToString();
+            var expected = "Please enter a year after 1582";
+            Assert.Contains(expected,actual);
         }
     }
 }
